249bc5991d0e372a04e435ac09b3d53a9d6f9624
Jaden Eckel <jadeneckel@gmail.com>
2021-09-01 09:57:05 -0400

    rough basics of site

--- public/js/comic_settings.js
+++ public/js/comic_settings.js
@@ -9,14 +9,14 @@ let pg = Number(findGetParameter("pg")); //make "pg" mean the current page numbe
 ////////////////////////
 
 //REALLY IMPORTANT ONES
-const maxpg = 14; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
+const maxpg = 17; //the current number of pages your comic has in total. this DOESNT necessarily mean number of IMAGE FILES as it doesn't count pages split into multiple files. 
 //MUST UPDATE NUMBER MANUALLY EVERY TIME YOU ADD A NEW PAGE or else it wont display the most recent page
 
 // COMIC PAGE SETTINGS
 const folder = "img/comics"; //directory of the folder where you keep all the comics
-const image = "pg"; //what you'll name all your comic pages
-const imgPart = "_" //special character(s) you put after the page number to subdivide pages into multiple image files (ie pg2_1, pg2_2, etc)
-const ext = "jpg"; //file extension of your comic pages
+const image = "strip"; //what you'll name all your comic pages
+const imgPart = " pg" //special character(s) you put after the page number to subdivide pages into multiple image files (ie pg2_1, pg2_2, etc)
+const ext = "png"; //file extension of your comic pages
 
 //NAVIGATION SETTINGS
 const navText = ["First","Previous","Next","Last"]; //alt text for your nav images, or just the text that shows up if you're not using images
@@ -41,65 +41,139 @@ if (pg == 0) {pg = maxpg;} //display MOST RECENT COMIC when the webpage is loade
 const pgData = [
     {
         pgNum: 1, //what page number it is
-        title: "The First Page Title", //the title of the page (leaving this blank will default it to "Page X")
-        date: "March 16, 2021", //the date on which the page was posted (mainly for the archive)
-        altText: "Here's some alt text!", //the alt text (mouse over text) for this particular comic. put nothing inbetween the quotes for no alt text
-        imageFiles: 1, //how many image files this page is split into
-        authorNotes: `
-            <p>If you want to write an author notes section, this'd be the place to do it.</p>
-            <p>You can even use whatever html tags you want in here to format it, the script called on your html page should spit out anything you type between these backticks.</p>
-            `
+        title: "Starting A Band", //the title of the page (leaving this blank will default it to "Page X")
+        date: "April 16, 2021", //the date on which the page was posted (mainly for the archive)
+        altText: "", //the alt text (mouse over text) for this particular comic. put nothing inbetween the quotes for no alt text
+        imageFiles: 3, //how many image files this page is split into
+        authorNotes: ``,
     },
     {
         pgNum: 2,
-        title: "The Second Page Title",
-        date: "March 17, 2021",
-        altText: "Here's some more alt text!",
-        imageFiles: 2,
-        authorNotes: `
-            <p>You can have different author notes for every page.</p>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris vulputate, orci sit amet dignissim eleifend, magna felis malesuada nunc, ut sagittis purus mi ac urna. Fusce ligula urna, varius vel sapien sit amet, vulputate tempor felis. In hac habitasse platea dictumst. Aliquam laoreet volutpat interdum. Vestibulum non libero sit amet leo accumsan porttitor. Vivamus nec porttitor neque. Sed eget mauris quam.</p>
-            `
+        title: "Showing Each Other Music",
+        date: "Apr 16, 2021",
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
     },
     {
         pgNum: 3,
-        title: "The Third Page Title",
-        date: "March 18, 2021",
-        altText: "Here's even more alt text!",
-        imageFiles: 1,
-        authorNotes: `
-            <p>Sed lectus magna, dignissim eu sapien quis, euismod pulvinar diam. In odio massa, auctor blandit dolor id, varius ultricies lacus. Suspendisse sed libero vel leo dictum consectetur. In fringilla elit sit amet placerat varius. Duis vel lacus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Proin eleifend metus eu ex elementum venenatis. Curabitur sodales, ipsum placerat ornare convallis, sem eros convallis felis, vel efficitur erat ante id diam.</p>
-            `
+        title: "Naming Their Band",
+        date: "Mar 18, 2021",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
     },
     {
         pgNum: 4,
-        title: "Even If The Title of a Page Is Really Long, It'll Wrap",
-        date: "March 19, 2021",
-        altText: "So much alt text...",
-        imageFiles: 1,
-        authorNotes: `
-            <p>Sed lectus magna, dignissim eu sapien quis, euismod pulvinar diam. In odio massa, auctor blandit dolor id, varius ultricies lacus. Suspendisse sed libero vel leo dictum consectetur. In fringilla elit sit amet placerat varius. Duis vel lacus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Proin eleifend metus eu ex elementum venenatis. Curabitur sodales, ipsum placerat ornare convallis, sem eros convallis felis, vel efficitur erat ante id diam.</p>
-            `
+        title: "Discovering New Music",
+        date: "May 4, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
     },
-        {
+    {
         pgNum: 5,
-        title: "Also if you don't feel like coming up with a title for every page, you don't have to.",
-        date: "March 20, 2021",
-        altText: "Here's even more alt text!",
+        title: "Recording Vocals",
+        date: "May 11, 2020",
+        altText: "",
         imageFiles: 1,
-        authorNotes: `
-            <p>Sed lectus magna, dignissim eu sapien quis, euismod pulvinar diam. In odio massa, auctor blandit dolor id, varius ultricies lacus. Suspendisse sed libero vel leo dictum consectetur. In fringilla elit sit amet placerat varius. Duis vel lacus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Proin eleifend metus eu ex elementum venenatis. Curabitur sodales, ipsum placerat ornare convallis, sem eros convallis felis, vel efficitur erat ante id diam.</p>
-            `
+        authorNotes: ``,
     },
-        {
+    {
         pgNum: 6,
-        title: `Unnamed pages won't display a title, and they'll show up as "Page [X]" when listed in the archive`,
-        date: "March 21, 2021",
-        altText: "Here's even more alt text!",
+        title: "Making a Short Song",
+        date: "May 18, 2020",
+        altText: "",
         imageFiles: 1,
-        authorNotes: `
-            <p>Sed lectus magna, dignissim eu sapien quis, euismod pulvinar diam. In odio massa, auctor blandit dolor id, varius ultricies lacus. Suspendisse sed libero vel leo dictum consectetur. In fringilla elit sit amet placerat varius. Duis vel lacus ante. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Nulla facilisi. Proin eleifend metus eu ex elementum venenatis. Curabitur sodales, ipsum placerat ornare convallis, sem eros convallis felis, vel efficitur erat ante id diam.</p>
-            `
+        authorNotes: ``,
+    },
+    {
+        pgNum: 7,
+        title: "Vs. Nardwuar (PART 1)",
+        date: "May 25, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 8,
+        title: "Vs. Nardwuar (PART 2)",
+        date: "May 25, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 9,
+        title: "Vs. Nardwuar (PART 3)",
+        date: "Jun 8, 2020",
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 10,
+        title: "Vs. Nardwuar (PART 4)",
+        date: "Jun 15, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 11,
+        title: "Posting on /mu/",
+        date: "Jun 22, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 12,
+        title: "Watching Bohemian Rhapsody",
+        date: "Jun 29, 2020",
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 13,
+        title: "Learning Music Theory",
+        date: "Jul 13, 2020",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 14,
+        title: "Learning Music Theory (Addendum)",
+        date: "Jul 20, 2020",
+        altText: "",
+        imageFiles: 1,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 15,
+        title: "Singing To God (PART 1)",
+        date: "Aug 11, 2020",
+        altText: "",
+        imageFiles: 3,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 16,
+        title: "Singing To God (PART 2)",
+        date: "Apr 26, 2021",
+        altText: "",
+        imageFiles: 3,
+        authorNotes: ``,
+    },
+    {
+        pgNum: 17,
+        title: "Singing To God (PART 3)",
+        date: "May 3, 2021",
+        altText: "",
+        imageFiles: 2,
+        authorNotes: ``,
     },
 ];
 
